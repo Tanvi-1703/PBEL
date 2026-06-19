affbc8cfa4176b4fd34a0c871dc580eaada05bcc
Tanvi <tanvigjl17@gmail.com>
2026-06-19 15:08:44 +0530

    1. added basic.py

---
+++ basic.py
@@ -0,0 +1,18 @@
+#variable
+a= 10 #integer variable
+print(a) #print the value of a #dynamic language
+print(type(a)) #print the type of a
+
+a= 10.5 #float variable
+a= 'Hello' #string variable
+a= True #boolean variable
+a= 1+1j #complex variable
+a= 1,2,3,4 #tuple variable
+a= [1,2,3,4] #list variable
+a= {1,2,3,4} #set variable
+a={1:'one',2:'two',3:'three'} #dictionary variable
+
+
+class Myclass:
+    pass
+
